18d43bd38e22742c187a2f3ce0307ca1950ec50c
Sera_Is <isonserena@hotmail.it>
2021-04-02 23:39:08 +0200

    Created project.html

---
+++ app.js
@@ -0,0 +1,48 @@
+function playWithMe() {
+    let userChoice = prompt("Challenge accepted 😉. Pick your choice: rock, paper, scissors");
+    userChoice = userChoice.toLowerCase();
+    userChoice = userChoice.trim();
+    let options = ["rock", "paper", "scissors"];
+    if (!options.includes(userChoice)) {
+        alert("That's not a valid choice... Are you getting cold feet? 😇");
+    } else {
+        let myChoice = Math.random();
+        if (myChoice < 0.34) {
+            myChoice = "rock";
+        } else if (myChoice <= 0.67) {
+            myChoice = "paper";
+        } else {
+            myChoice = "scissors";
+        }
+        compare(userChoice, myChoice);
+    }
+};
+function compare(choice1, choice2) {
+    if (choice1 === choice2) {
+        alert("It's a tie! We are both pretty good at this 🙌🏽");
+    } else {
+        if (choice1 === "rock") {
+            if (choice2 === "scissors") {
+                alert("Rock wins! You are good! 🤓");
+            } else {
+                alert("Paper wins! Try again! You might win next time 😛");
+            }
+        }
+        if (choice1 === "paper") {
+            if (choice2 === "rock") {
+                alert("Paper wins! You are good! 🤓");
+            } else {
+                alert("Scissors win! Try again! You might win next time 😛");
+            }
+        }
+        if (choice1 === "scissors") {
+            if (choice2 === "paper") {
+                alert("Scissors win! You are good! 🤓");
+            } else {
+                alert("Rock wins! Try again! You might win next time 😛");
+            }
+        }
+    }
+}
+let game = document.getElementById("challenge");
+game.addEventListener("click", playWithMe);
